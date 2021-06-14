9eec80cd1ca2be2d25bb4a4080956b1714a2a4a7
josedoce <josesamya@gmail.com>
2021-06-14 17:10:36 -0300

    app: rotas da aplicação

---
+++ src/routes/index.tsx
@@ -0,0 +1,18 @@
+import {
+BrowserRouter as Router,
+Switch,
+Route,
+} from 'react-router-dom';
+import Admin from '../views/Admin';
+import Home from '../views/Home';
+
+export default function Routes(){
+  return(
+    <Router>
+      <Switch>
+        <Route path="/" exact component={Home}/>
+        <Route path="/admin" component={Admin}/>
+      </Switch>
+    </Router>
+  )
+}
